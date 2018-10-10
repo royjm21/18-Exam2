34871a1bc9d682c94b875784f10c9cfbe496847e
royjm <royjm@rose-hulman.edu>
2018-10-09 20:49:49 -0400

    1a

--- src/problem1.py
+++ src/problem1.py
@@ -2,8 +2,8 @@
 Exam 2, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, their colleagues,
-         and PUT_YOUR_NAME_HERE.  October 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Jeremy Roy.  October 2018.
+"""  # TODOne: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -48,7 +48,7 @@ def main():
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
 
-    # run_test_problem1a()
+    run_test_problem1a()
     # run_test_problem1b()
     # run_test_problem1c()
     # run_test_problem1d()
@@ -218,9 +218,14 @@ def problem1a(strings):
       :rtype: list of int
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # TODOne: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    sequence = []
+    for k in range(len(strings)):
+        string = strings[k]
+        sequence = sequence + [len(string)]
+    return sequence
 
 
 def run_test_problem1b():
